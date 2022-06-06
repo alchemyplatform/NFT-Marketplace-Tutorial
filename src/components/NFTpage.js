eb37e85a778b4606687b007a77e011862bf1b845
Aniket Gupta <aniketguptaknp@gmail.com>
2022-06-06 13:43:53 -0700

    Add design for NFT page

--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -1,9 +1,26 @@
+import Navbar from "./Navbar";
+import axie from "../tile.jpeg";
+
 function NFTPage () {
     return(
         <div>
-            <img src="" alt="" />
-            <div>
-                Description
+            <Navbar></Navbar>
+            <div className="flex ml-20 mt-20">
+                <img src={axie} alt="" className="w-48 md:w-72" />
+                <div className="text-xl ml-5 text-white">
+                    <div>
+                        Name: {"Axie#4563"}
+                    </div>
+                    <div>
+                        Description: {"Axie Infinity Marketplace"}
+                    </div>
+                    <div>
+                        <a href="htps://axieinfinity.com">Website</a>
+                    </div>
+                    <div>
+                        Price: {"0.03 ETH"}
+                    </div>
+                </div>
             </div>
         </div>
     )
