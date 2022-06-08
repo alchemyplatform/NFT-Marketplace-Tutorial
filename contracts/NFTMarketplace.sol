0ab32e0b2ef7b7eb8f0588e9631c3e1aaffd3f8d
Aniket Gupta <aniketguptaknp@gmail.com>
2022-06-08 16:39:38 -0700

    Finalize smart contract, add fetch functions and an execute sale function

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -10,20 +10,22 @@ contract NFTMarketplace is ERC721URIStorage {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
     address payable owner;
     uint256 listPrice = 0.01 ether;
 
     struct ListedToken {
         uint256 tokenId;
-        uint256 price;
         address payable owner;
+        address payable seller;
+        uint256 price;
         bool currentlyListed;
     }
 
     event TokenListedSuccess (
         uint256 indexed tokenId,
-        address seller,
         address owner,
+        address seller,
         uint256 price,
         bool currentlyListed
     );
@@ -48,18 +50,21 @@ contract NFTMarketplace is ERC721URIStorage {
         return idToListedToken[currentTokenId];
     }
 
+    function getListedTokenForId(uint256 tokenId) public view returns (ListedToken memory) {
+        return idToListedToken[tokenId];
+    }
+
     function getCurrentToken() public view returns (uint256) {
         return _tokenIds.current();
     }
 
+    //The first time a token is created, it is listed here
     function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
         _tokenIds.increment();
-
         uint256 newTokenId = _tokenIds.current();
 
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
-
         createListedToken(newTokenId, price);
 
         return newTokenId;
@@ -71,26 +76,30 @@ contract NFTMarketplace is ERC721URIStorage {
 
         idToListedToken[tokenId] = ListedToken(
             tokenId,
-            price,
             payable(address(this)),
+            payable(msg.sender),
+            price,
             true
         );
 
         _transfer(msg.sender, address(this), tokenId);
         emit TokenListedSuccess(
             tokenId,
-            msg.sender,
             address(this),
+            msg.sender,
             price,
             true
         );
     }
-
+    
+    //This will return all the NFTs currently listed to be sold on the marketplace
     function getAllNFTs() public view returns (ListedToken[] memory) {
         uint nftCount = _tokenIds.current();
         ListedToken[] memory tokens = new ListedToken[](nftCount);
         uint currentIndex = 0;
 
+        //at the moment currentlyListed is true for all, if it becomes false in the future we will 
+        //filter out currentlyListed == false over here
         for(uint i=0;i<nftCount;i++)
         {
             uint currentId = i + 1;
@@ -101,4 +110,47 @@ contract NFTMarketplace is ERC721URIStorage {
         return tokens;
     }
     
+    //Returns all the NFTs that the current user is owner or seller in
+    function getMyNFTs() public view returns (ListedToken[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for(uint i=0; i < totalItemCount; i++)
+        {
+            if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender){
+                itemCount += 1;
+            }
+        }
+
+        ListedToken[] memory items = new ListedToken[](itemCount);
+        for(uint i=0; i < totalItemCount; i++) {
+            if(idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender) {
+                uint currentId = i+1;
+                ListedToken storage currentItem = idToListedToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    function executeSale(uint256 tokenId) public payable {
+        uint price = idToListedToken[tokenId].price;
+        address seller = idToListedToken[tokenId].seller;
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        //idToListedToken[tokenId].owner = payable(msg.sender);
+        idToListedToken[tokenId].currentlyListed = true;
+        idToListedToken[tokenId].seller = payable(msg.sender);
+        _itemsSold.increment();
+
+        _transfer(address(this), msg.sender, tokenId);
+        approve(address(this), tokenId);
+        payable(owner).transfer(listPrice);
+        payable(seller).transfer(msg.value);
+    }
+
+    //We might add a resell token function in the future
+    //In that case, tokens won't be listed by default but users can send a request to actually list a token
 }
