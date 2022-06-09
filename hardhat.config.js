7a1ac6d6d929bbfa0baf0699cc439a498539ead8
Aniket Gupta <aniketguptaknp@gmail.com>
2022-06-09 14:36:22 -0700

    remove rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,10 +25,6 @@ module.exports = {
       url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
       //accounts: [process.env.privateKey]
     },
-    rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/w0nWEXALCJhqpb-evR3BtHeiTt2Fy57w",
-      accounts: [ "b4bbbbefa56b673d92a78084961af52c4b3e5df37a8a19027ef440e176a9b0b9" ]
-    },
     goerli: {
       url: "https://eth-goerli.alchemyapi.io/v2/08I5O9tb_qOFPJAhRE8kUOxhEtS-DERr",
       accounts: [ "b4bbbbefa56b673d92a78084961af52c4b3e5df37a8a19027ef440e176a9b0b9" ]
